410c4672083e598d2b2ba8a23eed92aa8c8a4b07
ad1tyaSharma <adihgr18@gmail.com>
2023-02-22 13:42:11 +0530

    .

--- users/views.py
+++ users/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import userForm
 # Create your views here.
 def loginView(request):
     return render(request,'users/login.html')
 def registerView(request):
-    return render(request,'users/register.html')
+    if request.method == 'POST':
+        form = userForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.likes = []
+            post.save()
+            return redirect('users/login')
+    else:
+        form = userForm()
+
+    return render(request,'users/register.html',{'form': form})
 def forgotPassView(request):
     return render(request,'users/forgotPass.html')
 def profileView(request):
