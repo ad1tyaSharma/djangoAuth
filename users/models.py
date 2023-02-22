8e3498ab4d5e5efa265918589849b91f6ca702a8
ad1tyaSharma <adihgr18@gmail.com>
2023-02-22 13:42:19 +0530

    .

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    role = models.CharField(default='blogger',max_length=50)
+    password = models.SlugField()
+    profilePic = models.URLField(default='https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png')
